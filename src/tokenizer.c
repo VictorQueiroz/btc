2e2ea99494ce371cedbbb58dc33194fa06eff184
Victor Queiroz <victorcqueirozg@gmail.com>
2018-05-13 21:48:24 -0300

    src/tokenizer.c: add tokenizer implementation to handle comma

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -90,8 +90,8 @@ int btc_tokenizer_is_punctuator(btc_tokenizer* tokenizer) {
     if(ch == 0x7b || ch == 0x7d)
         return 1;
 
-    // : or ;
-    if(ch == 0x3a || ch == 0x3b)
+    // : or ; or , 
+    if(ch == 0x3a || ch == 0x3b || ch == 0x2c)
         return 1;
 
     // ? or -
