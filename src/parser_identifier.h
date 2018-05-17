eca515004565469762a39e97ea80f8d4b810ac9e
Victor Queiroz <victorcqueirozg@gmail.com>
2018-05-16 21:42:06 -0300

    src/parser_identifier.h: declare `typedef` at the top of the header

--- src/parser_identifier.h
+++ src/parser_identifier.h
@@ -1,10 +1,10 @@
 #ifndef PARSER_IDENTIFIER_H_
 #define PARSER_IDENTIFIER_H_
 
+typedef struct _btc_ast_identifier btc_ast_identifier;
+
 struct _btc_ast_identifier {
     const char* value;
 };
-
-typedef struct _btc_ast_identifier btc_ast_identifier;
 
 #endif
