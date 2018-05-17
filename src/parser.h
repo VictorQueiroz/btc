6b8bcd938cbac59d8c748ca71896bed03638c0f3
Victor Queiroz <victorcqueirozg@gmail.com>
2018-05-16 21:42:52 -0300

    src/parser.h: use all ast items as separate and independent items as much as possible

--- src/parser.h
+++ src/parser.h
@@ -1,71 +1,19 @@
 #ifndef PARSER_H_
 #define PARSER_H_
 
-#include "tokenizer.h"
-#include "parser_identifier.h"
-
-#define BTC_CONTAINER_GROUP 1
-
-struct _btc_ast_container_param {
-    btc_ast_identifier name;
-    btc_ast_identifier type;
-    struct _btc_ast_container_param* next_param;
-    struct _btc_ast_container_param* previous_param;
-};
-
-typedef struct _btc_ast_container_param btc_ast_container_param;
-
-typedef struct {
-    btc_ast_container_param* first_param;
-    btc_ast_container_param* last_param;
-} btc_ast_container_params;
-
-struct _btc_ast_container_declaration {
-    btc_ast_identifier name;
-    btc_ast_identifier type;
-    btc_ast_container_params* body;
-};
-
-typedef struct _btc_ast_container_declaration btc_ast_container_declaration;
-
-typedef struct _btc_linked_container_declaration {
-    btc_ast_container_declaration container;
-    struct _btc_linked_container_declaration* next_item;
-    struct _btc_linked_container_declaration* previous_item;
-} btc_linked_container_declaration;
-
-typedef struct {
-    btc_linked_container_declaration* first_item;
-    btc_linked_container_declaration* last_item;
-} btc_ast_containers_list; 
-
-typedef struct {
-    btc_ast_identifier type;
-    /**
-     * Common list of container declarations without `type` 
-     * param
-     */
-    btc_ast_containers_list* body;
-} btc_ast_container_group_declaration;
-
-struct _btc_ast_item {
-    int type;
-
-    btc_ast_container_group_declaration container_group;
-
-    struct _btc_ast_item* next_item;
-    struct _btc_ast_item* previous_item;
-};
-
-typedef struct _btc_ast_item btc_ast_item;
-
-struct _btc_parser {
-    btc_ast_item* last_item;
-    btc_ast_item* first_item;
-    btc_token* current_token;
-};
+#define BTC_OK 1
 
 typedef struct _btc_parser btc_parser;
+
+#include "tokenizer.h"
+#include "ast_item.h"
+#include "container_group.h"
+#include "parser_identifier.h"
+
+struct _btc_parser {
+    btc_ast_list* result;
+    btc_token* current_token;
+};
 
 void btc_parser_init(btc_parser** parser_ptr, btc_tokenizer* tokenizer);
 void btc_parser_destroy(btc_parser* parser);
@@ -74,6 +22,8 @@ int btc_parser_eof(btc_parser* parser);
 
 int btc_parser_peek_and_consume(btc_parser* parser, const char* string);
 
-#endif
+btc_ast_identifier btc_parser_consume_identifier(btc_parser* parser);
+void btc_parser_scan_namespace(btc_parser* parser, btc_ast_item* result);
+void btc_parser_scan_type_group_definition(btc_parser* parser, btc_ast_item* result);
 
 #endif
