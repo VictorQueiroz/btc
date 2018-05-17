8fcfa7c175ff4eef357879b8919b114df65b3968
Victor Queiroz <victorcqueirozg@gmail.com>
2018-05-17 18:14:28 -0300

    src/ast/ast_item.c: add break after BTC_NAMESPACE statement is processed

--- src/ast/ast_item.c
+++ src/ast/ast_item.c
@@ -20,6 +20,7 @@ void btc_destroy_ast_item(btc_ast_item* item) {
             break;
         case BTC_NAMESPACE:
             btc_destroy_namespace(item->namespace);
+            break;
         case BTC_IMPORT_DECLARATION:
         case BTC_IDENTIFIER:
             break;
