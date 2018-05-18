2d33d2ea2c51999153772eb4a39bf188e7b5823d
Victor Queiroz <victorcqueirozg@gmail.com>
2018-05-17 21:25:35 -0300

    src/ast/template.c: use `sizeof(btc_template)` instead of wrongly written `sizeof(tmpl_ptr)`

--- src/ast/template.c
+++ src/ast/template.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 
 void btc_initialize_template(btc_template** tmpl_ptr) {
-    *tmpl_ptr = calloc(1, sizeof(tmpl_ptr));
+    *tmpl_ptr = calloc(1, sizeof(btc_template));
     btc_create_ast_list(&(*tmpl_ptr)->arguments);
 }
 
