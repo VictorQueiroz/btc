dd2ddc512924244b1c740d6d0f21aa331e1f7630
Victor Queiroz <victorcqueirozg@gmail.com>
2018-05-27 17:28:21 -0300

    src/ast/comments_list.c: improve comments list api

--- src/ast/comments_list.h
+++ src/ast/comments_list.h
@@ -2,10 +2,9 @@
 #define COMMENTS_LIST_H_
 
 typedef struct _btc_comments_list btc_comments_list;
+typedef struct _btc_linked_comment btc_linked_comment;
 
-struct _btc_comment {
-	const char* value;
-};
+#include "comment.h"
 
 struct _btc_linked_comment {
 	struct _btc_comment comment;
@@ -14,8 +13,11 @@ struct _btc_linked_comment {
 };
 
 struct _btc_comments_list {
-	struct _btc_linked_comment* first_comment;
-	struct _btc_linked_comment* last_comment;
+	struct _btc_linked_comment* first_item;
+	struct _btc_linked_comment* last_item;
 };
+
+void btc_add_comment(btc_comments_list* list, btc_comment comment);
+void btc_initialize_comments_list(btc_comments_list** list_ptr);
 
 #endif
