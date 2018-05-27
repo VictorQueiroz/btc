668dd425c2f164ab0eff49d26839a785d8d3986c
Victor Queiroz <victorcqueirozg@gmail.com>
2018-05-27 17:12:40 -0300

    src/token.h: create `_btc_token_range` struct

--- src/token.h
+++ src/token.h
@@ -2,6 +2,18 @@
 #define BTC_TOKEN_H_
 
 typedef struct _btc_token btc_token;
+typedef struct _btc_token_range btc_token_range;
+
+struct _btc_token_range {
+	// Start line number
+	int start_line_number;
+	// End line number
+	int end_line_number;
+	// Start offset
+	int start_offset;
+	// End offset
+	int end_offset;
+};
 
 struct _btc_token {
     int type;
@@ -9,6 +21,8 @@ struct _btc_token {
     const char* value;
     char* allocated;
     double number;
+
+    struct _btc_token_range range;
 };
 
 void btc_destroy_token(btc_token* token);
