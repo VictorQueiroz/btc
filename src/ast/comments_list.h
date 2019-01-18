e0ec1df1003236f3154c7bbbfe9c9159760ae153
Victor Queiroz <victorcqueirozg@gmail.com>
2019-01-18 11:43:31 -0300

    src/ast/comments_list.h: use btc_comment direct instead of pointing to a struct

--- src/ast/comments_list.h
+++ src/ast/comments_list.h
@@ -1,8 +1,10 @@
 #ifndef COMMENTS_LIST_H_
 #define COMMENTS_LIST_H_
 
+#include "comment.h"
+
 #include <cvector.h>
 
-CVECTOR_HEAD(btc_comments_list, struct _btc_comment*)
+CVECTOR_HEAD(btc_comments_list, btc_comment*)
 
 #endif
