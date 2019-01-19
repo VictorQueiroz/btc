9b042ef566404f1690d3ee5b89eceac8394f674e
Victor Queiroz <victorcqueirozg@gmail.com>
2019-01-19 10:30:56 -0300

    create btc_template_declaration struct

---
+++ src/ast/template_declaration.h
@@ -0,0 +1,9 @@
+#ifndef BTC_TEMPLATE_DECLARATION_H
+#define BTC_TEMPLATE_DECLARATION_H
+
+typedef struct {
+    btc_ast_item* arguments;
+    btc_ast_item* body;
+} btc_template_declaration;
+
+#endif //BTC_TEMPLATE_DECLARATION_H
